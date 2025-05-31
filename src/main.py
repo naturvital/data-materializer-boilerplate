25157b2b3be86c1294cfe94e55fd7fb3182a43a5
Pablo Pinargote <pablo.pinargote@outlook.com>
2025-05-31 14:56:40 -0500

    Adding Dockerfile

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import logging
+import sys
 from google.cloud import bigquery
 
 
 # Configure logging at module level
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s %(levelname)s %(message)s"
+    format="%(asctime)s %(levelname)s %(message)s",
+    stream=sys.stdout
 )
 logger = logging.getLogger(__name__)
 
